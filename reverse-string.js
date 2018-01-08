51c866b58c998eea696506f1b2d61b16810ba77e
Naim Jeem <naim36208@gmail.com>
2018-01-08 23:30:14 +0600

    Reverse string

---
+++ reverse-string.js
@@ -0,0 +1,6 @@
+const reaverseString = (string) => {
+	return string.split('').reverse().join('');
+};
+console.log('-----------------------------');
+console.log(reaverseString('Reverse String'));
+console.log('-----------------------------');
